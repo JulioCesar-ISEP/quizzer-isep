3260106c8f29578fd8373b49eb6446c13f2b4013
julio-cesar <1180024@isep.ipp.pt>
2025-11-11 17:21:10 +0000

    deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quizzer-isep/', // IMPORTANTE: substitua pelo nome do seu repositório
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
-    host: true,
-    port: 3000
+    port: 3000,
   }
 })
