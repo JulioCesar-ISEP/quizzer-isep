b5b8cd106393b73cc6ddf2cf0340af5edcd6d07b
julio-cesar <1180024@isep.ipp.pt>
2025-11-26 16:30:22 +0000

    refactor: pré-deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quizzer-isep/', // IMPORTANTE: substitua pelo nome do seu repositório
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
